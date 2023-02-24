32519b7d68aa4fe584b4cff9bd40c9aeef371577
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2023-02-24 12:22:09 +0100

    Added logic to check if a WholeNumber is found in a record where a DecimalNumber is expected. Added & inplemented logic to convert said WholeNumber to DecimalNumber

--- src/main/java/org/statsserver/domain/KeyDataList.java
+++ src/main/java/org/statsserver/domain/KeyDataList.java
@@ -1,52 +1,79 @@
 package org.statsserver.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import lombok.Getter;
 import org.statsserver.util.ValueDataTypeService;
 
-import java.util.*;
-
 public class KeyDataList {
-    private Set<String> keysList = new LinkedHashSet<String>();
-    @Getter
-    private HashMap<String, KeyData> keyDataList = new HashMap<String, KeyData>();
+  private Set<String> keysList = new LinkedHashSet<String>();
+  @Getter private HashMap<String, KeyData> keyDataList = new HashMap<String, KeyData>();
 
-    public KeyDataList() {
+  public KeyDataList() {}
 
-    }
-    public void addKeyAndDataType(String keyName, Object value) throws Exception {
-        if(!keyName.isEmpty() && value != null){
-            if(!this.keysList.contains(keyName)){
-                this.keysList.add(keyName);
-                this.keyDataList.put(keyName, new KeyData(keyName, value));
-            }else{
-                this.keyDataList.get(keyName).updateKeyData(keyName, value);
-            }
-
-            if(!this.isValueSameKeyEqualDataType(keyName, ValueDataTypeService.getValueDataType(value))){
-                throw new Exception("Key: "+keyName+" already exists in dataList with type: "+this.keyDataList.get(keyName).getKeyDataAsResponseKeyData().getDataType()+" but new entry type: "+value+" creates conflict");
-            }
+  public void addKeyAndDataType(String keyName, Object value) throws Exception {
+    if (!keyName.isEmpty() && value != null) {
+      if (!this.keysList.contains(keyName)) {
+        this.keysList.add(keyName);
+        this.keyDataList.put(keyName, new KeyData(keyName, value));
+      } else {
+        if (this.keyDataList.get(keyName).getValueType().equals("DecimalNumber")
+            && this.isValueOfType(value, "WholeNumber")) {
+          value = ValueDataTypeService.convertWholeNumberToDecimalNumber(value);
         }
-    }
+        this.keyDataList.get(keyName).updateKeyData(keyName, value);
+      }
 
-    public ArrayList<HashMap<String, Object>> getAllKeysAndDataTypes(){
-        ArrayList<HashMap<String, Object>> keyAndDataTypeList = new ArrayList<>();
-        this.keyDataList.forEach((key, value) -> {
-            keyAndDataTypeList.add(value.getKeyDataAsResponseKeyData().getResponseKeyDataAsHashMap());
+      if (!this.isValueSameKeyEqualDataType(
+          keyName, ValueDataTypeService.getValueDataType(value))) {
+        throw new Exception(
+            "Key: "
+                + keyName
+                + " already exists in dataList with type: "
+                + this.keyDataList.get(keyName).getKeyDataAsResponseKeyData().getDataType()
+                + " but new entry type: "
+                + ValueDataTypeService.getValueDataType(value)
+                + ", value: ("
+                + value
+                + ") creates conflict");
+      }
+    }
+  }
+
+  private boolean isValueOfType(Object value, String type) {
+    String valueType = ValueDataTypeService.getValueDataType(value);
+    return valueType.equals(type);
+  }
+
+  public ArrayList<HashMap<String, Object>> getAllKeysAndDataTypes() {
+    ArrayList<HashMap<String, Object>> keyAndDataTypeList = new ArrayList<>();
+    this.keyDataList.forEach(
+        (key, value) -> {
+          keyAndDataTypeList.add(value.getKeyDataAsResponseKeyData().getResponseKeyDataAsHashMap());
         });
-        return keyAndDataTypeList;
-    }
-    public boolean getKeyExists(String keyName) {
-        return this.keysList.contains(keyName);
-    }
-    private boolean isValueSameKeyEqualDataType(String keyName, Object dataTypeOfValue) {
-        //get current entry type
-        // is equal to new entry value type, return true
-        // if not, return false
-        boolean result = this.keyDataList.get(keyName).getKeyDataAsResponseKeyData().getDataType().equals(dataTypeOfValue);
-        return result;
-    }
+    return keyAndDataTypeList;
+  }
 
-    public KeyData getKey(String keyName) {
-        return this.keyDataList.get(keyName);
-    }
+  public boolean getKeyExists(String keyName) {
+    return this.keysList.contains(keyName);
+  }
+
+  private boolean isValueSameKeyEqualDataType(String keyName, Object dataTypeOfValue) {
+    // get current entry type
+    // is equal to new entry value type, return true
+    // if not, return false
+    boolean result =
+        this.keyDataList
+            .get(keyName)
+            .getKeyDataAsResponseKeyData()
+            .getDataType()
+            .equals(dataTypeOfValue);
+    return result;
+  }
+
+  public KeyData getKey(String keyName) {
+    return this.keyDataList.get(keyName);
+  }
 }
--- src/main/java/org/statsserver/util/ValueDataTypeService.java
+++ src/main/java/org/statsserver/util/ValueDataTypeService.java
@@ -1,43 +1,48 @@
 package org.statsserver.util;
 
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import static org.statsserver.util.DateChecker.isValidDate;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class ValueDataTypeService {
-  public static String getValueDataType(Object value){
-    if(String.class.equals(value.getClass())){
+  public static String getValueDataType(Object value) {
+    // todo: should refactor these data types to enums
+    if (String.class.equals(value.getClass())) {
       String stringValue = (String) value;
-      if(isValidDate(stringValue)){
-//                System.out.println("DateString");
+      if (isValidDate(stringValue)) {
+        //                System.out.println("DateString");
         return "DateString";
       }
-//            System.out.println("String");
+      //            System.out.println("String");
       return "String";
     } else if (Integer.class.equals(value.getClass()) || Long.class.equals(value.getClass())) {
-//            System.out.println("Integer");
+      //            System.out.println("Integer");
       return "WholeNumber";
-    } else if (Double.class.equals(value.getClass())) {
-//            System.out.println("Double");
+    } else if (Double.class.equals(value.getClass()) || Float.class.equals(value.getClass())) {
+      //            System.out.println("Double");
       return "DecimalNumber";
     } else if (Boolean.class.equals(value.getClass())) {
-//            System.out.println("Boolean");
+      //            System.out.println("Boolean");
       return "Boolean";
     } else if (ArrayList.class.equals(value.getClass())) {
-//            System.out.println("List");
+      //            System.out.println("List");
       return "List";
     } else if (Map.class.equals(value.getClass()) || LinkedHashMap.class.equals(value.getClass())) {
-//            System.out.println("Map");
+      //            System.out.println("Map");
       return "Map";
     }
     System.out.println("DEBUG: OTHER");
     return "OTHER";
+  }
+
+  public static Object convertWholeNumberToDecimalNumber(Object value) {
+    DecimalFormat decimalFormattedValue =
+        new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.ENGLISH));
+    return Double.valueOf(decimalFormattedValue.format(value));
   }
 }
